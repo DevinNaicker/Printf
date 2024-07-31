677b5ac135df2ce43ffa9d3b287417c79af62ea0
Devin Naicker <devinnaicker@gmail.com>
2024-07-31 12:01:57 -0700

    0v4

--- printf.c
+++ printf.c
@@ -9,96 +9,92 @@
  * Return: number of chars printed.
  */
 
+/* Function to print a character */
 void print_char(char c, int *count)
 {
-	_putchar(c);
-	(*count)++;
+    _putchar(c);
+    (*count)++;
 }
 
+/* Function to print a string */
 void print_str(char *str, int *count)
 {
-	if (str)
-	{
-		while (*str)
-		{
-			_putchar(*str++);
-			(*count)++;
-		}
-	}
-
-	else
-	{
-		char *null_str = "(null)";
-
-		while (*null_str)
-		{
-			_putchar(*null_str++);
-			(*count)++;
-		}
-	}
+    if (str)
+    {
+        while (*str)
+        {
+            _putchar(*str++);
+            (*count)++;
+        }
+    }
+    else
+    {
+        char *null_str = "(null)";
+        while (*null_str)
+        {
+            _putchar(*null_str++);
+            (*count)++;
+        }
+    }
 }
 
-int hndl_format(const char *format, va_list args, int *count, unsigned int *i)
+/* Function to handle format specifiers */
+int handle_format(const char *format, va_list args, int *count, unsigned int *i)
 {
-	(*i)++;
-
-	if (format[*i] == '\0')
-	{
-		return (-1);
-	}
-
-	switch (format[*i])
-	{
-		case 'c':
-			print_char(va_arg(args, int), count);
-			break;
-
-		case 's':
-			print_str(va_arg(args, char *), count);
-			break;
-
-		case '%':
-			print_char('%', count);
-			break;
-		default:
-			print_char('%', count);
-			print_char(format[*i], count);
-			(*count)++;
-			break;
-	}
-
-	return (0);
+    (*i)++;
+    if (format[*i] == '\0')
+    {
+        return -1;
+    }
+    switch (format[*i])
+    {
+        case 'c':
+            print_char((char)va_arg(args, int), count);
+            break;
+        case 's':
+            print_str(va_arg(args, char *), count);
+            break;
+        case '%':
+            print_char('%', count);
+            break;
+        default:
+            print_char('%', count);
+            print_char(format[*i], count);
+            (*count)++; // The unknown format specifier counts as 1 char
+            break;
+    }
+    return 0;
 }
 
+/* Main custom printf function */
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0;
-	int count = 0;
-	va_list args;
+    unsigned int i = 0;
+    int count = 0;
+    va_list args;
 
-	if (!format)
-		return (-1);
+    if (!format)
+        return (-1);
 
-	va_start(args, format);
+    va_start(args, format);
 
-	while (format && format[i])
-	{
-		if (format[i] == '%')
-		{
-			if (hndl_format(format, args, &count, &i) == -1)
-			{
-				va_end(args);
-				return (-1);
-			}
-		}
+    while (format[i])
+    {
+        if (format[i] == '%')
+        {
+            if (handle_format(format, args, &count, &i) == -1)
+            {
+                va_end(args);
+                return (-1);
+            }
+        }
+        else
+        {
+            print_char(format[i], &count);
+        }
+        i++;
+    }
 
-		else
-		{
-			print_char(format[i], &count);
-		}
-		i++;
-	}
-
-	va_end(args);
-	return (count);
+    va_end(args);
+    return (count);
 }
