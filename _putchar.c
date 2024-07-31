60a68886b564c93375b82deb34c6ffab0ca5d63e
OrapelengN <portiaoran@gmail.com>
2024-07-31 10:33:18 -0700

    printf.c

--- _putchar.c
+++ _putchar.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <unistd.h>
+#include <stdarg.h>
 
 /**
  * _putchar - writes the character c to stdout
---
+++ main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+int main(void)
+{
+    _printf("Character: %c\n", 'H');
+    _printf("String: %s\n", "Hello, World!");
+    _printf("Percentage: %%\n");
+    _printf("Binary: %b\n", 5); /* Should print 101 */
+
+    return (0);
+}
