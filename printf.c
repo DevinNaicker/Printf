546ad2acc5b99855e5f9ef975011eac57a5f66bf
Devin Naicker <devinnaicker@gmail.com>
2024-07-31 09:02:46 -0700

    0

---
+++ printf.c
@@ -0,0 +1,80 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...)
+{
+	unsigned int i = 0;
+	int count = 0;
+	va_list args;
+
+	if (!format)
+		return (-1);
+
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == '\0')
+			{
+				va_end(args);
+				return (-1);
+			}
+
+			switch (format[i])
+			{
+				case 'c':
+					_putchar(va_arg(args, int));
+					count++;
+					break;
+				case 's':
+					{
+						char *str = va_arg(args, char *);
+						if (str)
+						{
+							while (*str)
+							{
+								_putchar(*str++);
+								count++;
+							}
+						}
+
+						else
+						{
+							char *null_str = "(null)";
+							while (*null_str)
+							{
+								_putchar(*null_str++);
+								count++;
+							}
+						}
+						break;
+					}
+				
+				case '%':
+					_putchar('%');
+					count++;
+					break;
+				
+				default:
+					_putchar('%');
+					_putchar(format[i]);
+					count += 2;
+					break;
+			}
+		}
+
+		else
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		i++;
+	}
+
+	va_end(args);
+	return (count);
+}
