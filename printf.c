113537caf2abca590ece4a3c722461501a5b4ac3
Devin Naicker <devinnaicker@gmail.com>
2024-07-31 12:13:12 -0700

    0v6

--- printf.c
+++ printf.c
@@ -9,64 +9,6 @@
  * Return: number of chars printed.
  */
 
-/* Function to print a character */
-void print_char(char c, int *count)
-{
-    _putchar(c);
-    (*count)++;
-}
-
-/* Function to print a string */
-void print_str(char *str, int *count)
-{
-    if (str)
-    {
-        while (*str)
-        {
-            _putchar(*str++);
-            (*count)++;
-        }
-    }
-    else
-    {
-        char *null_str = "(null)";
-        while (*null_str)
-        {
-            _putchar(*null_str++);
-            (*count)++;
-        }
-    }
-}
-
-/* Function to handle format specifiers */
-int handle_format(const char *format, va_list args, int *count, unsigned int *i)
-{
-    (*i)++;
-    if (format[*i] == '\0')
-    {
-        return -1;
-    }
-    switch (format[*i])
-    {
-        case 'c':
-            print_char((char)va_arg(args, int), count);
-            break;
-        case 's':
-            print_str(va_arg(args, char *), count);
-            break;
-        case '%':
-            print_char('%', count);
-            break;
-        default:
-            print_char('%', count);
-            print_char(format[*i], count);
-            (*count)++;
-            break;
-    }
-    return 0;
-}
-
-/* Main custom printf function */
 int _printf(const char *format, ...)
 {
     unsigned int i = 0;
@@ -78,19 +20,66 @@ int _printf(const char *format, ...)
 
     va_start(args, format);
 
-    while (format[i])
+    while (format && format[i])
     {
         if (format[i] == '%')
         {
-            if (handle_format(format, args, &count, &i) == -1)
+            i++;
+            if (format[i] == '\0')
             {
                 va_end(args);
                 return (-1);
             }
+
+            switch (format[i])
+            {
+                case 'c':
+                    _putchar(va_arg(args, int));
+                    count++;
+                    break;
+                case 's':
+                    {
+                        char *str = va_arg(args, char *);
+
+                        if (str)
+                        {
+                            while (*str)
+                            {
+                                _putchar(*str++);
+                                count++;
+                            }
+                        }
+
+                        else
+                        {
+                            char *null_str = "(null)";
+
+                            while (*null_str)
+                            {
+                                _putchar(*null_str++);
+                                count++;
+                            }
+                        }
+                        break;
+                    }
+
+                case '%':
+                    _putchar('%');
+                    count++;
+                    break;
+
+                default:
+                    _putchar('%');
+                    _putchar(format[i]);
+                    count += 2;
+                    break;
+            }
         }
+
         else
         {
-            print_char(format[i], &count);
+            _putchar(format[i]);
+            count++;
         }
         i++;
     }
