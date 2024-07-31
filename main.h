6bfbaa89e2354e78324fcad826c8f237e3b7ac94
OrapelengN <portiaoran@gmail.com>
2024-07-31 13:05:59 -0700

    Print Binary 0.4

--- main.h
+++ main.h
@@ -6,20 +6,6 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-/**
- * struct format - to convert for printf
- * @str: declaring a char pointer variable
- * @func: conversion function for the specifier
- *
- */
-
-typedef struct format
-{
-char *str;
-int (*func)();
-}
-convert;
-
 int _putchar(char c);
 int _printf(const char *format, ...);
 int print_binary(va_list val);
