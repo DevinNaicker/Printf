08a038c2bfc0ef5d30e139a11368470ba47de843
Devin Naicker <devinnaicker@gmail.com>
2024-07-31 11:07:07 -0700

    0v2

--- printf.c
+++ printf.c
@@ -9,6 +9,67 @@
  * Return: number of chars printed.
  */
 
+void print_char(char c, int *count)
+{
+	_putchar(c);
+	(*count)++;
+}
+
+void print_str(char *str, int *count)
+{
+	if (str)
+	{
+		while (*str)
+		{
+			_putchar(*str++);
+			(*count)++;
+		}
+	}
+
+	else
+	{
+		char *null_str = "(null)";
+		
+		while (*null_str)
+		{
+			_putchar(*null_str++);
+			(*count)++;
+		}
+	}
+}
+
+int handle_format(const char *format, va_list args, int *count, unsigned int *i)
+{
+	(*i)++;
+
+	if (format[*i] == '\0')
+	{
+		return -1;
+	}
+
+	switch (format[*i])
+	{
+		case 'c':
+			print_char(va_arg(args, int), count);
+			break;
+		
+		case 's':
+			print_str(va_arg(args, char *), count);
+			break;
+
+		case '%':
+			print_char('%', count);
+			break;
+		default:
+			print_char('%', count);
+			print_char(format[*i], count);
+			(*count)++;
+			break;
+	}
+
+	return 0;
+}
+
 int _printf(const char *format, ...)
 {
 	unsigned int i = 0;
@@ -24,62 +85,16 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] == '%')
 		{
-			i++;
-			if (format[i] == '\0')
+			if (handle_format(format, args, &count, &i) == -1)
 			{
 				va_end(args);
 				return (-1);
-			}
-
-			switch (format[i])
-			{
-				case 'c':
-					_putchar(va_arg(args, int));
-					count++;
-					break;
-				case 's':
-					{
-						char *str = va_arg(args, char *);
-
-						if (str)
-						{
-							while (*str)
-							{
-								_putchar(*str++);
-								count++;
-							}
-						}
-
-						else
-						{
-							char *null_str = "(null)";
-
-							while (*null_str)
-							{
-								_putchar(*null_str++);
-								count++;
-							}
-						}
-						break;
-					}
-
-				case '%':
-					_putchar('%');
-					count++;
-					break;
-
-				default:
-					_putchar('%');
-					_putchar(format[i]);
-					count += 2;
-					break;
 			}
 		}
 
 		else
 		{
-			_putchar(format[i]);
-			count++;
+			print_char(format[i], &count);
 		}
 		i++;
 	}
