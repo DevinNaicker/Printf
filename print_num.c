65850b040752b48244d9ed29bae4ad2a64ffb029
Devin Naicker <devinnaicker@gmail.com>
2024-07-31 16:32:32 -0700

    1

--- print_num.c
+++ print_num.c
@@ -9,48 +9,49 @@
  * Return nothing
  */
 
-int print_number(int n) {
-    char buffer[12];
-    int i = 0, j, count = 0;
-    int is_negative = 0;
+int print_number(int n)
+{
+	char buffer[12];
+	int i = 0, j, count = 0;
+	int is_negative = 0;
 
-    if (n == -2147483648)
-    {
-        _putchar('-');
-        _putchar('2');
-        n = 147483648;
-        count = 2;
-    }
+	if (n == -2147483648)
+	{
+		_putchar('-');
+		_putchar('2');
+		n = 147483648;
+		count = 2;
+	}
 
-    if (n == 0)
-    {
-        _putchar('0');
-        return 1;
-    }
+	if (n == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
 
-    if (n < 0)
-    {
-        is_negative = 1;
-        n = -n;
-    }
+	if (n < 0)
+	{
+		is_negative = 1;
+		n = -n;
+	}
 
-    while (n > 0)
-    {
-        buffer[i++] = (n % 10) + '0';
-        n /= 10;
-    }
+	while (n > 0)
+	{
+		buffer[i++] = (n % 10) + '0';
+		n /= 10;
+	}
 
-    if (is_negative && count == 0)
-    {
-        _putchar('-');
-        count++;
-    }
+	if (is_negative && count == 0)
+	{
+		_putchar('-');
+		count++;
+	}
 
-    for (j = i - 1; j >= 0; j--)
-    {
-        _putchar(buffer[j]);
-        count++;
-    }
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(buffer[j]);
+		count++;
+	}
 
-    return count;
+	return (count);
 }
