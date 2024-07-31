8cb9049e3cc7bf70f9bfe10847ef09f595895e03
Portia Orapeleng Nkwane <159598611+OrapelengN@users.noreply.github.com>
2024-08-01 01:45:01 +0200

    Update printf.c

--- printf.c
+++ printf.c
@@ -12,12 +12,12 @@
 
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0;
-	int count = 0;
-	va_list args;
+unsigned int i = 0;
+int count = 0;
+va_list args;
 
-	if (!format)
-		return (-1);
+if (!format)
+return (-1);
 
 	va_start(args, format);
 
